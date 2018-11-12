a78b82e59c2d16aa286d93cae0f84297e6801473
Vincent Demeester <vincent@sbr.pm>
2018-11-12 17:17:25 +0100

    travis: more fixes 😱
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- hack/push.sh
+++ hack/push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     docker push docker.io/vdemeester/kobw-base
     docker push docker.io/vdemeester/kobw-builder
